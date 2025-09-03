99059fa3aaa8220cdd5dbf7b4137011ad4e99768
Eliabe Albuquerque <eliabesantos214@gmail.com>
2025-09-02 23:36:35 -0300

    atualizar versão 1.1.1

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 4000;
-const VERSION = process.env.APP_VERSION || '1.1.0';
+const VERSION = process.env.APP_VERSION || '1.1.1';
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: `Olá do backend! Versão ${VERSION}` });
